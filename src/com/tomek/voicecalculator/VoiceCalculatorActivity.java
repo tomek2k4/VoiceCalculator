094cef5cce8cb1432c569f4ee50ac445ab460a34
Tomek <tomasz.maslon@gmail.com>
2017-01-19 23:21:01 +0100

    Added speech recognition feature

--- src/com/tomek/voicecalculator/VoiceCalculatorActivity.java
+++ src/com/tomek/voicecalculator/VoiceCalculatorActivity.java
@@ -1,19 +1,28 @@
 package com.tomek.voicecalculator;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import com.tomek.voicecalculator.calculator.CalculatorLib;
+import com.tomek.voicecalculator.calculator.CalculatorUtills;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class VoiceCalculatorActivity extends Activity {
 
+	private final int SPEECH_RECOGNITION_CODE = 1;
+	
 	EditText input;
 	TextView output;
-	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +35,64 @@ public class VoiceCalculatorActivity extends Activity {
     
     
     public void onClickGo(View view){
-    	String expr = input.getText().toString();
-    	if(expr!=null && expr.length()!=0){
-    		expr += "=";
-    		CalculatorLib calcLib = new CalculatorLib();
-    		calcLib.enter(expr);
-    		String result = calcLib.getOutput(); 		
-    		output.append(String.format("\n%s%s",expr,result));
-    		calcLib.dispose();	
-    	}
-    	
+    	startSpeechToText();
     }
+    
+	/**
+	 * Start speech to text intent. This opens up Google Speech Recognition API dialog box to listen the speech input.
+	 * */
+	 private void startSpeechToText() {
+		 input.setText("");
+		 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+		 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		 intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+				"Speak something...");
+		 try {
+			 startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+		 } catch (ActivityNotFoundException a) {
+			 Toast.makeText(getApplicationContext(),
+					"Sorry! Speech recognition is not supported in this device.",
+					Toast.LENGTH_SHORT).show();
+		}
+	 }
+
+	 /**
+	  * Callback for speech recognition activity
+	  * */
+	 @Override
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		 super.onActivityResult(requestCode, resultCode, data);
+
+		 switch (requestCode) {
+		 case SPEECH_RECOGNITION_CODE: {
+			 if (resultCode == RESULT_OK && null != data) {
+
+				 ArrayList<String> result = data
+						 .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+				 String text = result.get(0);
+				 input.setText(text);
+				 String equation = CalculatorUtills.prepareExpression(text);
+				 String calcResult = calculate(equation);
+		    	 output.append(String.format("\n%s%s",equation,calcResult));
+			 }
+			 break;
+		 }
+
+		 }
+	 }
+
+	 private String calculate(String equation){
+		 String result = "";
+		 if(equation!=null && equation.length()!=0){
+	    		CalculatorLib calcLib = new CalculatorLib();
+	    		calcLib.enter(equation);
+	    		result = calcLib.getOutput(); 		
+	    		calcLib.dispose();	
+	     }
+		 return result;
+	 }
+    
 
 }
---
+++ src/com/tomek/voicecalculator/calculator/CalculatorUtills.java
@@ -0,0 +1,93 @@
+package com.tomek.voicecalculator.calculator;
+
+public class CalculatorUtills {
+	
+	private final static String WHITESPACE_REGEX = "\\s+";
+	private final static String NUMERIC_REGEX = "\\d+";
+	
+	public static String prepareExpression(String input){
+		
+		String expresion = transform(input);
+		expresion += "=";
+		
+		return expresion;
+	}
+
+	private static String transform(String inp){
+		String[] splitStr = inp.split(WHITESPACE_REGEX);
+		StringBuilder strBuilder = new StringBuilder();
+		for(String word:splitStr){
+			strBuilder.append(translateWord(word));
+		}
+		
+		return strBuilder.toString();
+	}
+	
+	private static String translateWord(String word){
+		if(word.matches(NUMERIC_REGEX)){
+			return word;
+		}else if(word.length() == 1 && "+-*/".indexOf(word)>=0){
+			return word;
+		}
+		
+		String result = word;
+		word = word.toLowerCase();
+		switch (word) {
+		case "zero":
+			result = "0";
+			break;
+		case "null":
+			result = "0";
+			break;
+		case "one":
+			result = "1";
+			break;
+		case "two":
+			result = "2";
+			break;
+		case "three":
+			result = "3";
+			break;
+		case "four":
+			result = "4";
+			break;
+		case "five":
+			result = "5";
+			break;
+		case "six":
+			result = "6";
+			break;
+		case "seven":
+			result = "7";
+			break;
+		case "eight":
+			result = "8";
+			break;
+		case "nine":
+			result = "9";
+			break;
+		case "plus":
+			result = "+";
+			break;
+		case "minus":
+			result = "-";
+			break;
+		case "multiply":
+			result = "*";
+			break;
+		case "times":
+			result = "*";
+			break;
+		case "x":
+			result = "*";
+			break;
+		case "equals":
+			result = "";
+			break;
+		default:
+			result = "0";
+			break;
+		}
+		return result;
+	}
+}
